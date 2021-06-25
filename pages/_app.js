396f14c358fa60e4d12ff21201bebf44cd510341
laur80 <laur80raul@gmail.com>
2021-06-25 15:21:00 +0300

    bootstrap

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+// import '../styles.css';
+export default function MyApp({ Component, pageProps }) {
+	// Your custom stuff here
+
+	return <Component {...pageProps} />;
+}
